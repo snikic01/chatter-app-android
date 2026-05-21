45457a056a2d61f50732b6aeefb98f8a9f2cfe9c
snikic01 <90110060+snikic01@users.noreply.github.com>
2026-05-21 09:36:46 +0200

    Dodate rute za privatni cet

--- app/src/main/java/com/example/chatterapp/data/NetworkConfig.kt
+++ app/src/main/java/com/example/chatterapp/data/NetworkConfig.kt
@@ -29,5 +29,23 @@ object NetworkConfig {
 
     fun getSearchUsersUrl(groupId: Int, query: String): String = "${BASE_URL}api_groups.php?action=search_users&group_id=$groupId&query=$query"
 
+    // ================= RUTA ZA PRIVATNE PORUKE 1-NA-1 =================
+
+    // 1. URL za povlačenje spiska svih privatnih četova (korisnika, zadnje poruke i crvenih balončića)
+    fun getPrivateChatsUrl(username: String): String =
+        "${BASE_URL}api_private.php?action=list&username=$username"
+
+    // 2. URL za povlačenje kompletne istorije privatnih poruka sa određenim prijateljem
+    fun getPrivateChatUrl(username: String, chatUserId: Int): String =
+        "${BASE_URL}api_private.php?action=fetch&username=$username&chat_user_id=$chatUserId"
+
+    // 3. Centralni API URL za slanje novih privatnih poruka (POST zahtev)
+    fun getPrivateSendApiUrl(): String =
+        "${BASE_URL}api_private.php"
+
+    // 4. Centralni API URL za označavanje privatnih poruka kao pročitanih (POST zahtev)
+    fun getPrivateSeenApiUrl(): String =
+        "${BASE_URL}api_private.php"
+
 
 }
